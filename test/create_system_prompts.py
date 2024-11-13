1018bbce02e4e313bc0402b61d412d688f4fc7f5
milantru1 <milantru1@gmail.com>
2024-11-13 15:20:00 +0100

    Update script for generating system prompts

--- test/create_system_prompts.py
+++ test/create_system_prompts.py
@@ -2,17 +2,15 @@ import argparse
 import pandas as pd
 from ydata_profiling import ProfileReport
 import json
+from pathlib import Path
 
-
-parser = argparse.ArgumentParser(description="Script to generate a data profiling report and prompts.")
-# TODO check args (path to dataset default? encoding ok? addit. info and user view default to "")
-parser.add_argument("-p", "--path_to_dataset", type=str, required=True, help="Path to the dataset file")
-parser.add_argument("-s", "--separator", type=str, default=";", help="Separator used in the dataset")
-parser.add_argument("-e", "--encoding", type=str, default="utf-8", help="Encoding used in the dataset")
-parser.add_argument("-g", "--generate_html", type=bool, default=False, help="Flag for enabling HTML report generation")
-parser.add_argument("-a", "--additional_info", type=str, default="", help="Path to the dataset file")
-parser.add_argument("-u", "--user_view", type=str, default="", help="Path to the dataset file")
-
+parser = argparse.ArgumentParser(description="Script to generate a data profiling report and prompts for system prompting phase.")
+parser.add_argument("-p", "--path_to_datasets", type=str, required=True, help="Path to the dataset folder.")
+parser.add_argument("-s", "--separator", type=str, default=",", help="Separator used in the dataset.")
+parser.add_argument("-e", "--encoding", type=str, default="utf-8", help="Encoding used in the dataset.")
+parser.add_argument("-g", "--generate_html", type=bool, default=False, help="Flag for enabling HTML report generation.")
+parser.add_argument("-a", "--additional_info", type=str, default="", help="Additional information about the dataset.")
+parser.add_argument("-u", "--user_view", type=str, default="", help="User view of the dataset.")
 
 def to_percentage(num):
     return round(num * 100, 2)
@@ -93,7 +91,7 @@ def add_columns_info_to_knowledge(dataset_knowledge, report):
                     col_i_name = col_j_name
                     continue
                 if corr_value > corr_threshold:
-                    cols_correlated_with_col_i.append(col_j_name)
+                    cols_correlated_with_col_i.append(f"{col_j_name} (correlation value: {corr_value})")
             processed_columns_data[col_i_name]["Is correlated with columns"] = cols_correlated_with_col_i
     
     # Finally, update knowledge
@@ -108,7 +106,7 @@ def add_general_table_info_to_knowledge(dataset_knowledge, report):
     dataset_knowledge["Missing cells count"] = table_data["n_cells_missing"]
     dataset_knowledge["Missing cells count in %"] = to_percentage(table_data["p_cells_missing"])
 
-def create_dataset_knowledge(args, report):
+def create_dataset_knowledge(report):
     dataset_knowledge = {} # We will gradually add new knowledge about the dataset (new properties)
     
     add_general_table_info_to_knowledge(dataset_knowledge, report)
@@ -116,35 +114,29 @@ def create_dataset_knowledge(args, report):
     
     return dataset_knowledge
 
-def create_instructions_prompt():
-    return '''\
+def create_instructions_prompt(csv_files_paths):
+    def get_string(csv_files_names):
+        csv_files_count = len(csv_files_names)
+        if csv_files_count == 1:
+            return f"1 table: {csv_files_names[0]}"
+        else:
+            tmp = ", ".join(csv_files_names)
+            return f"{csv_files_count} tables: {tmp}"
+    csv_files_names = [csv_file_path.name for csv_file_path in csv_files_paths]
+    return f'''\
 INSTRUCTIONS:
-- You are part of an application called CSV Oracle, designed to assist software engineers in understanding their data.
-- Your role is to act as an assistant, helping software engineers comprehend the dataset they provide and assess its suitability for their projects.
-- Every information about the dataset is considered to be a DATASET INFORMATION FRAGMENT.
-- There are two phases: SYSTEM PROMPT PHASE and USER PROMPT PHASE.
-- SYSTEM PROMPT PHASE:
-    - In this phase, you will receive pre-made prompts from the system.
-    - You will first be provided with a sample data and possibly schema, additional information about the dataset, user view, or output from data profiling.
-    - You will then answer questions about the dataset, aiming to extract as much information as possible. Each answer will be considered a DATASET INFORMATION FRAGMENT.
-- USER PROMPT PHASE:
-    - In this phase, you will communicate directly with the user, answering their questions about the dataset and the problems they want to solve.
-    - Your responses may or may not introduce new DATASET INFORMATION FRAGMENTs.
-    - User might want to update user notes (add, remove, or rewrite notes). If user wants to edit notes your message should follow format: `{Your answer to the user}\n\n[LLM COMMAND] {Your instruction to LLM editing notes}`.
-    - When using tag `[LLM COMMAND]` in your message, the answer to the user MUST be present as well. 
-- EVERY TIME your answer contains DATASET INFORMATION FRAGMENT, add `[FRAGMENT SPAWNED]` at the beginning of your message.
-- Tag `[FRAGMENT SPAWNED]` can be only at the beginning of the message, nowhere else.
-- A message can contain only one tag, it CANNOT contain more than one tag. For example, if your message includes `[LLM COMMAND]`, it cannot also contain `[FRAGMENT SPAWNED]`, and vice versa. Another example: if your mesage includes `[FRAGMENT SPAWNED]`, it cannot contain another `[FRAGMENT SPAWNED]` (the same goes for `[LLM COMMAND]`).
-- Every DATASET INFORMATION FRAGMENT and every LLM COMMAND will be send to the another LLM editing notes.
-- You MUST NEVER mention anything related to the second LLM editing notes, the tags `[FRAGMENT SPAWNED]` or `[LLM COMMAND]`, or the SYSTEM PROMPT PHASE and USER PROMPT PHASE.
-- The language of the dataset may differ from that of the user, who is expected to speak English. You will answer questions about the dataset in English by default.
-- The SYSTEM PROMPT PHASE concludes when you receive the message: "SYSTEM PROMPT PHASE is ending, USER PROMPT PHASE starts after this message." After this message, the USER PROMPT PHASE begins and continues for the remainder of the conversation. The user CANNOT revert to the SYSTEM PROMPT PHASE.
+- You function as a tool for software engineers to answer questions about the dataset.
+- First you will be given information about the dataset. Information like sample data and possibly schema, additional information about the dataset, user view, or output from data profiling.
+- Then you will be prompted with questions about the dataset and you will answer them, aiming to extract as much information as possible.
+- Your answers should be concise and to the point, containing only the answer to the question. So no other text, like for example "Feel free to ask me if you have more questions...".
+- The dataset consists of {get_string(csv_files_names)}.
+- The language of the dataset may differ from that of the user, who is expected to speak English. Because of that, you will answer questions about the dataset in English by default.
 
 If you understand, please respond with "OK".\
 '''
 
-def create_data_prompt(schema, sample_data, additional_info, user_view, data_profiling_output):
-    prompt_with_input = ""
+def create_data_prompt(csv_file_name, schema, sample_data, additional_info, user_view, data_profiling_output):
+    prompt_with_input = f"Information about {csv_file_name}:\n"
 
     if schema: prompt_with_input += f'''Schema:\n"""\n{schema}\n"""\n\n'''
     prompt_with_input += f'''Sample data:\n"""\n{sample_data}\n"""\n\n'''
@@ -155,58 +147,36 @@ def create_data_prompt(schema, sample_data, additional_info, user_view, data_pro
     
     return prompt_with_input 
 
-def create_prompts(args, sample_data, dataset_knowledge):
-    prompts = [
-        create_instructions_prompt(),
-        create_data_prompt(
-            schema=None, # TODO (schema) try this also with schema
-            sample_data=sample_data,
-            additional_info=args.additional_info,
-            user_view=args.user_view,
-            data_profiling_output=dataset_knowledge
-        ),
-        "Summarize what the dataset represents and explain what context or domain the data comes from. Be concise.",
-        "What kind of entity or entities does the table row represent? Make the answer concise.",
-    ]
-
-    for col_name, col_info in dataset_knowledge["Columns"].items():
-        column_prompt = f'Provide a brief description of the column {col_name}. What does it describe or represent? Answer only with the column description, no other text (tags are allowed).'
+def add_column_prompts(prompts, csv_file_name, columns_knowledge):
+    for col_name, col_info in columns_knowledge.items():
+        column_prompt = f'Provide a brief description of the column {col_name} from table {csv_file_name}. What does it describe or represent? Answer only with the column description, no other text (tags are allowed).'
         prompts.append(column_prompt)
 
         # "Missing values count" is expected to always be in col_info, but we check its existience anyway 
         # because of defensive programming. And we do so in case of other properties as well.
-        if "Missing values count" in col_info and col_info["Missing values count"] > 0:
-            column_prompt_missing_values = f'Provide an explanation as to why the values are missing in the column {col_name}. Answer only with the explanation, no other text (tags are allowed).'
+        if ("Missing values count" in col_info and col_info["Missing values count"] > 0) \
+        and ("Missing values count in %" in col_info and col_info["Missing values count in %"] > 0):
+            column_prompt_missing_values = f'The column {col_name} from table {csv_file_name} has {col_info["Missing values count"]} missing values ({col_info["Missing values count in %"]} %). Provide an explanation why the values are missing. Answer only with the explanation, no other text (tags are allowed).'
             prompts.append(column_prompt_missing_values)
         
         if "Is correlated with columns" in col_info and len(col_info["Is correlated with columns"]) > 0:
             for correlated_col_name in col_info["Is correlated with columns"]:
-                column_prompt_corr = f'Provide an explanation for why the column {col_name} is correlated with the column {correlated_col_name}. Answer only with the explanation, no other text (tags are allowed).'
+                # correlated_col_name includes, apart from the col name, the corr value
+                column_prompt_corr = f'Provide an explanation for why the column {col_name} from table {csv_file_name} is correlated with the column {correlated_col_name}. Answer only with the explanation, no other text (tags are allowed).'
                 prompts.append(column_prompt_corr)
 
         # TODO (schema) for each column, if schema provided; chcelo by to schemu...
         # column_prompt_schema = 'Why does this constraint exist? Explain the reasoning behind the given constraint or rule in the schema.'
         # prompts.append(column_prompt_schema)
 
-    tmp = f'User view for the dataset was provided. User view:\n"""\n{args.user_view}\n"""\n\nIf you can answer the user need coming from the user view, write the answer as if you were writing it to the user. Otherwise just write "Hello! How can I help you with this dataset?".' if args.user_view else 'Just write "Hello! How can I help you with this dataset?"'
-    end_prompt = f'''\
-SYSTEM PROMPT PHASE is ending, USER PROMPT PHASE starts after this message.
-
-{tmp}
-
-DO NO FORGET ABOUT THE TAGS AND USE THEM ACCORDING TO THE INSTRUCTIONS!\
-'''
-    prompts.append(end_prompt)
-
-    return prompts
-
-def create_data_profiling_report(args):
-    df = pd.read_csv(args.path_to_dataset, sep=args.separator, encoding=args.encoding)
+def create_data_profiling_report(args, csv_file_path):
+    df = pd.read_csv(csv_file_path, sep=args.separator, encoding=args.encoding)
 
     type_schema = None # {"Survived": "categorical", "Embarked": "categorical"} # TODO (schema) Schema?
     profile = ProfileReport(df, title="CSV Oracle profiling report using ydata profiling", type_schema=type_schema)
     if args.generate_html:
-        profile.to_file(r"reports\report.html")
+        csv_filename = csv_file_path.stem
+        profile.to_file(rf"reports\report_for_{csv_filename}.html")
 
     json_string = profile.to_json()
     report = json.loads(json_string)
@@ -214,15 +184,47 @@ def create_data_profiling_report(args):
     return report
 
 def main(args):
-    report = create_data_profiling_report(args)
+    datasets_folder_path = Path(args.path_to_datasets)
+    csv_files_paths = list(datasets_folder_path.glob("*.csv"))
 
-    dataset_knowledge = create_dataset_knowledge(args, report)
-    sample_data = create_sample_data(report, sep=args.separator)
+    prompts = [create_instructions_prompt(csv_files_paths)]
 
-    prompts = create_prompts(args, sample_data, dataset_knowledge)
+    datasets_columns_knowledge = []
+    for csv_file_path in csv_files_paths:
+        report = create_data_profiling_report(args, csv_file_path)
+
+        dataset_knowledge = create_dataset_knowledge(report)
+        datasets_columns_knowledge.append(dataset_knowledge["Columns"])
+        sample_data = create_sample_data(report, sep=args.separator)
+
+        data_prompt = create_data_prompt(
+            csv_file_name=csv_file_path.name,
+            schema=None, # TODO (schema) try this also with schema
+            sample_data=sample_data,
+            additional_info=args.additional_info,
+            user_view=args.user_view,
+            data_profiling_output=dataset_knowledge
+        )
+        prompts.append(data_prompt)
+    
+    # Questions start
+    tmp = " " if len(csv_files_paths) == 1 else " (as all tables together, prompts about each table separately will come later) "
+    prompts.append(f"Summarize what the dataset{tmp}represents and explain what context or domain the data comes from. Be concise.")
+
+    for i in range(len(csv_files_paths)):
+        csv_file_name = csv_files_paths[i].name
+        prompts.append(f"Summarize what the table {csv_file_name} represents and explain what context or domain the data comes from. Be concise.")
+        prompts.append(f"What kind of entity or entities does the table row of {csv_file_name} represent? Make the answer concise.")
+        dataset_columns_knowledge = datasets_columns_knowledge[i]
+        add_column_prompts(prompts, csv_file_name, dataset_columns_knowledge)
+
+    last_prompt = 'Just write "Hello! How can I help you with this dataset?"'
+    if args.user_view:
+        last_prompt = f'User view for the dataset was provided. User view:\n"""\n{args.user_view}\n"""\n\nIf you can answer the user need coming from the user view, write the answer as if you were writing it to the user (provide only answer, no questions, e.g. "Would you like assistance with some specific task?"). Otherwise just write "Hello! How can I help you with this dataset?".'
+    prompts.append(last_prompt)
 
     # TODO Remove writing prompts to file
-    with open("PROMPTS.md", "w", encoding="utf-8") as file:
+    with open("GENERATED_SYSTEM_PROMPTS.md", "w", encoding="utf-8") as file:
         file.write("```\n" + "\n```\n\n```\n".join(prompts) + "\n```\n")
 
 if __name__ == "__main__":
