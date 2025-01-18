aa51d50284eb962aae9afe96b6edcb2dbf22dd1f
milantru1 <milantru1@gmail.com>
2025-01-18 13:40:13 +0100

    Add interfaces

---
+++ CSVOracle/CSVOracle.Data/Interfaces/IDatasetRepository.cs
@@ -0,0 +1,14 @@
+﻿using CSVOracle.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSVOracle.Data.Interfaces
+{
+	public interface IDatasetRepository : IRepository<Dataset>
+	{
+		
+	}
+}
---
+++ CSVOracle/CSVOracle.Data/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSVOracle.Data.Interfaces
+{
+	public interface IRepository<TEntity> where TEntity : class, IEntity
+	{
+		public Task<TEntity?> GetAsync(int id);
+		public Task<IEnumerable<TEntity>> GetAsync(Func<TEntity, bool> predicate);
+		public Task<IEnumerable<TEntity>> GetAsync();
+		public Task AddAsync(TEntity entity);
+		public abstract Task UpdateAsync(TEntity entity);
+		public Task RemoveAsync(TEntity entity);
+	}
+}
