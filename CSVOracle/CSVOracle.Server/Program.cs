7ce3e122e75dad3a79537c52c33e975ca3e93965
milantru1 <milantru1@gmail.com>
2025-01-23 21:30:23 +0100

    Update Program.cs and .csproj

--- CSVOracle/CSVOracle.Server/Program.cs
+++ CSVOracle/CSVOracle.Server/Program.cs
@@ -1,20 +1,69 @@
 using CSVOracle.Data;
+using CSVOracle.Data.Interfaces;
+using CSVOracle.Data.Repositories;
+using CSVOracle.Server.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCors(options =>
+{
+	options.AddPolicy("AllowAllHeaders",
+		builder =>
+		{
+			builder
+			.AllowAnyOrigin()
+			.AllowAnyHeader()
+			.AllowAnyMethod();
+		});
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+	options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+	{
+		Description = "Standard Authorization header using the Bearer scheme  (\"bearer {token}\")",
+		In = ParameterLocation.Header,
+		Name = "Authorization",
+		Type = SecuritySchemeType.ApiKey
+	});
+
+	options.OperationFilter<SecurityRequirementsOperationFilter>();
+});
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+	.AddJwtBearer(options =>
+	{
+		options.TokenValidationParameters = new TokenValidationParameters
+		{
+			ValidateIssuerSigningKey = true,
+			IssuerSigningKey = new SymmetricSecurityKey(
+				key: Encoding.UTF8.GetBytes(builder.Configuration.GetRequiredSection("AppSettings:Token").Value!)
+			),
+			ValidateIssuer = false,
+			ValidateAudience = false
+		};
+	});
 
 builder.Services.AddDbContext<CSVOracleDbContext>(options =>
 {
-    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+	var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 	options.UseSqlServer(connectionString);
 });
+builder.Services.AddScoped<IDatasetRepository, DatasetRepository>();
+builder.Services.AddScoped<IChatRepository, ChatRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<TokenHelper>();
 
 var app = builder.Build();
 
@@ -24,11 +73,15 @@ app.UseStaticFiles();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+	app.UseSwagger();
+	app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection(); Problematic with CORS
+
+app.UseCors("AllowAllHeaders");
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
