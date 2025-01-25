1c635bb28ed91ef64ee81f3b0642a1a31122aa3f
milantru1 <milantru1@gmail.com>
2025-01-25 10:22:32 +0100

    Add profile and register page

---
+++ CSVOracle/csvoracle.client/src/pages/profile/Profile.tsx
@@ -0,0 +1,13 @@
+import UserForm from "../../shared/components/UserForm";
+
+function Profile() {
+	return (
+		<div>
+			<h1>Profile page</h1>
+
+			<UserForm />
+		</div>
+	);
+}
+
+export default Profile;
---
+++ CSVOracle/csvoracle.client/src/pages/register/Register.tsx
@@ -0,0 +1,13 @@
+import UserForm from "../../shared/components/UserForm";
+
+function Register() {
+	return (
+		<div>
+			<h2>Please fill in the register form</h2>
+
+			<UserForm />
+		</div>
+	);
+}
+
+export default Register;
