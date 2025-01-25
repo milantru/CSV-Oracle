22e11f16f40002946c9ca63fe0338848178d0432
milantru1 <milantru1@gmail.com>
2025-01-25 11:22:38 +0100

    Update Datasets page

--- CSVOracle/csvoracle.client/src/pages/datasets/Datasets.tsx
+++ CSVOracle/csvoracle.client/src/pages/datasets/Datasets.tsx
@@ -1,9 +1,53 @@
+import { useEffect, useState } from "react";
+import { Dataset } from "../../shared/types/Dataset";
+import { getUserDatasetsAPI } from "../../shared/services/DatasetServices";
+import { toast } from "react-toastify";
+import { Link } from "react-router-dom";
+
 function Datasets() {
+	const [datasets, setDatasets] = useState<Dataset[]>([]);
+
+	useEffect(() => {
+		async function fetchData() {
+			const { userDatasets, errorMessages } = await getUserDatasetsAPI()
+			if (errorMessages.length > 0) {
+				for (let i = 0; i < errorMessages.length; i++) {
+					toast.warning(errorMessages[i]);
+				}
+
+				return;
+			}
+
+			setDatasets(userDatasets);
+		}
+
+		fetchData();
+	}, []);
+
 	return (
 		<>
 			<h1>Datasets</h1>
 
-			<p>This is datasets page!</p>
+			<small>The dataset means the set of uploaded files (1 or more), not just one uploaded file.</small>
+			<h2>Select dataset...</h2>
+
+			<div>
+				<div>
+					<ul>
+						{datasets.map((dataset, index) => (
+							<li key={index}>Dataset with id {dataset.id}</li>
+						))}
+					</ul>
+				</div>
+				<div>
+					<div>
+						TODO: Dataset info
+					</div>
+				</div>
+				<div>
+					<Link to="/datasets/new">Add new dataset</Link>
+				</div>
+			</div>
 		</>
 	);
 }
