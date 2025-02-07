e8a78a113daf7de8bc8b895ec00c94d380f26213
milantru1 <milantru1@gmail.com>
2025-02-07 09:41:40 +0100

    Update generate_initial_dataset_knowledge.py

--- CSVOracle/CSVOracle.PythonScripts/generate_initial_dataset_knowledge.py
+++ CSVOracle/CSVOracle.PythonScripts/generate_initial_dataset_knowledge.py
@@ -2,13 +2,10 @@ import argparse
 import json
 import re
 from pathlib import Path
-from llama_index.core import StorageContext, BasePromptTemplate, load_index_from_storage
 from llama_index.core.tools import QueryEngineTool, ToolMetadata
-from llama_index.core.query_engine import SubQuestionQueryEngine
-from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.llms.ollama import Ollama
-from llama_index.core.schema import QueryBundle
-
+from llama_index.core.query_engine import SubQuestionQueryEngine
+from shared_helpers import get_file_paths, read_file, load_index, create_notes_llm, create_updated_dataset_knowledge, create_notes_llm_prompt
 
 parser = argparse.ArgumentParser(description="Script for generating an initial dataset knowledge representation.")
 parser.add_argument("-i", "--indices_folder_path", type=str, required=True, help="Path to the input folder containing index files (or to be more precise, index storage context dictionary files).")
@@ -17,54 +14,6 @@ parser.add_argument("-r", "--prompting_phase_instructions_path", type=str, requi
 parser.add_argument("-n", "--notes_llm_instructions_path", type=str, required=True, help="Path to the input file containing Notes LLM instructions prompt.")
 parser.add_argument("-o", "--output_file_path", type=str, required=True, help="Path to the file where the initial dataset knowledge representation should be stored. The new file will be created, or overwritten if already exists.")
 
-def get_file_paths(folder_path): # TODO move to shared funcs
-    return [file_path for file_path in folder_path.iterdir() if file_path.is_file()]
-
-def read_file(file_path, load_as_json=False): # TODO move to shared funcs
-    with open(file_path, "r") as f:
-        file_content = json.load(f) if load_as_json else f.read()
-    return file_content
-
-def load_index(index_storage_context_dict_path):
-    with open(index_storage_context_dict_path, "r") as index_storage_context_dict_file:
-        index_storage_context_dict = json.load(index_storage_context_dict_file)
-    index_storage_context = StorageContext.from_dict(index_storage_context_dict)
-    index = load_index_from_storage(index_storage_context, embed_model=HuggingFaceEmbedding()) # TODO maybe to shared (embed model)?
-    return index
-
-def extract_json_part(json_regex, text):
-    think_end_tag_index = text.find("</think>")
-    if think_end_tag_index != -1:
-        text = text[think_end_tag_index + len("</think>"):].lstrip()
-
-    match = json_regex.search(text)
-    if not match:
-        return None
-    tmp = match.group(1)
-    if tmp.startswith("```json\n"):
-        tmp = tmp[8:]
-    elif tmp.startswith("```json"):
-        tmp = tmp[7:]
-    if tmp.endswith("\n```"):
-        tmp = tmp[:-4]
-    elif tmp.endswith("```"):
-        tmp = tmp[:-4]
-
-    return tmp
-
-def create_notes_llm_prompt(dataset_knowledge, question, answer):
-    return f"""\
-CURRENT DATASET KNOWLEDGE:
-```json
-{dataset_knowledge}
-```
-
-INSTRUCTION SECTION:
-The answer for the question "{question}" was:
-"{answer}"
-
-Please, based on the provided answer, add the newly gained information to the CURRENT DATASET KNOWLEDGE.
-"""
 
 def generate_answer(query_engine, question, max_attempts_count = 3):
     attempts_count = 0
@@ -81,25 +30,6 @@ def generate_answer(query_engine, question, max_attempts_count = 3):
                 print(f"There was an error producing answer for question:\n'{question}'.")
                 return None
     return answer
-
-def create_updated_dataset_knowledge(notes_llm, notes_llm_instructions, json_regex, notes_llm_prompt, max_attempts_count = 3):
-    attempts_count = 0
-    while attempts_count < max_attempts_count:
-        try:
-            res = notes_llm.complete(notes_llm_instructions + notes_llm_prompt)
-            res_json = extract_json_part(json_regex, str(res))
-            if not res_json: raise Exception("JSON part not extracted.")
-            dataset_knowledge = json.loads(res_json)
-            break
-        except:
-            print(f"Failed creating updated dataset knowledge.")
-            attempts_count += 1
-            if attempts_count < max_attempts_count:
-                print("Retrying...")
-            else: 
-                print(f"Failed creating updated dataset knowledge for Notes LLM prompt:\n'{notes_llm_prompt}'.")
-                return None
-    return dataset_knowledge
 
 def create_query_engine_tools(index_storage_context_dicts_paths, llm):
     tool_metadata_provider = {
@@ -133,15 +63,6 @@ def create_prompting_phase_llm(prompting_phase_instructions):
         system_prompt=prompting_phase_instructions
     )
 
-def create_notes_llm(notes_llm_instructions):
-    return Ollama(
-        model="deepseek-r1:8b", 
-        request_timeout=420.0, 
-        # For some reason the system prompt is not working when LLM.complete() is used. That is why 
-        # I prepend instructions (system prompt) at the beginning of the prompt later when using Notes LLM.
-        # system_prompt=notes_llm_instructions
-    )
-
 def create_query_engine(index_storage_context_dicts_paths, llm):
     query_engine_tools = create_query_engine_tools(index_storage_context_dicts_paths, llm)
     
@@ -153,19 +74,27 @@ def create_query_engine(index_storage_context_dicts_paths, llm):
     
     return query_engine
 
+def create_instruction_for_notes_llm(question, answer):
+    return f"""\
+The answer for the question "{question}" was:
+"{answer}"
+
+Please, based on the provided answer, add the newly gained information to the CURRENT DATASET KNOWLEDGE.
+"""
+
 def create_initial_dataset_knowledge(prompting_phase_prompts, query_engine, notes_llm, notes_llm_instructions):    
-    json_regex = re.compile(r'```json([\s\S]*?)```')
+    json_regex = re.compile(r'```json([\s\S]*?)```') # perf improvement so we dont have to compile every iteration
     dataset_knowledge = '{\n"tables": []\n}'
     for prompting_phase_prompt in prompting_phase_prompts:
         answer = generate_answer(query_engine, prompting_phase_prompt)
         if not answer:
             continue
 
-        notes_llm_prompt = create_notes_llm_prompt(
-            dataset_knowledge, prompting_phase_prompt, answer.strip())
+        instruction = create_instruction_for_notes_llm(prompting_phase_prompt, answer.strip())
         # notes llm prompt contains current dataset knowledge
+        notes_llm_prompt = create_notes_llm_prompt(dataset_knowledge, instruction)
         updated_dataset_knowledge = create_updated_dataset_knowledge(
-            notes_llm, notes_llm_prompt, json_regex, notes_llm_instructions)
+            notes_llm, notes_llm_instructions + notes_llm_prompt, json_regex=json_regex)
         if not updated_dataset_knowledge:
             continue
 
