89ec889a498a0a948243d0ea315f78256d33e53a
milantru1 <milantru1@gmail.com>
2025-03-25 15:23:10 +0100

    Update scripts for running the app

--- run_app.dev.ps1
+++ run_app.dev.ps1
@@ -1,2 +1,2 @@
 Start-Process -NoNewWindow -FilePath "dotnet" -ArgumentList "watch --project=CSVOracle\CSVOracle.Server\"
-Start-Process -FilePath "python" -ArgumentList "-m flask --app .\CSVOracle\CSVOracle.PythonScripts\llm_server run --debug"
+Start-Process -FilePath "CSVOracle\CSVOracle.PythonScripts\venv\Scripts\python.exe" -ArgumentList "-m flask --app .\CSVOracle\CSVOracle.PythonScripts\llm_server run --debug"
--- run_app.ps1
+++ run_app.ps1
@@ -1,2 +1,2 @@
 Start-Process -NoNewWindow -FilePath "dotnet" -ArgumentList "run --project=CSVOracle\CSVOracle.Server\"
-Start-Process -NoNewWindow -FilePath "python" -ArgumentList "-m flask --app .\CSVOracle\CSVOracle.PythonScripts\llm_server run"
+Start-Process -NoNewWindow -FilePath "CSVOracle\CSVOracle.PythonScripts\venv\Scripts\python.exe" -ArgumentList "-m flask --app .\CSVOracle\CSVOracle.PythonScripts\llm_server run"
