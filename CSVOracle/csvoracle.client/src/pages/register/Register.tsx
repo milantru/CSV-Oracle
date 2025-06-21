8841b667d9850f042c044e3953c65b1c32700a33
milantru1 <milantru1@gmail.com>
2025-06-21 15:52:36 +0200

    Update margin in register page

--- CSVOracle/csvoracle.client/src/pages/register/Register.tsx
+++ CSVOracle/csvoracle.client/src/pages/register/Register.tsx
@@ -3,7 +3,7 @@ import UserForm from "../../shared/components/UserForm";
 function Register() {
 	return (
 		<>
-			<h1 className="text-center mb-2">Please fill in the register form</h1>
+			<h1 className="text-center my-4">Please fill in the register form</h1>
 
 			<UserForm />
 		</>
